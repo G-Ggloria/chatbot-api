105d3732d329313e2e215ccb5501743496bc3254
Si1ive <1479241132@qq.com>
2026-01-29 19:20:15 +0800

    feat:工程结构代码

---
+++ chatbot-api-interfaces/src/test/java/cn.bugstack.chatbot.api.test/ApiTest.java
@@ -0,0 +1,17 @@
+package cn.bugstack.chatbot.api.test;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+/**
+ * 单元测试
+ * */
+@SpringBootApplication
+public class ApiTest {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiTest.class,args);
+    }
+}
